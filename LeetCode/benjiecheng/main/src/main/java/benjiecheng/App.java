ba2b77793a4f4632266f4a171db7e6642b9b4a26
jykSS <5585776+jykSS@user.noreply.gitee.com>
2021-04-05 10:19:06 +0800

    benjiecheng

---
+++ LeetCode/benjiecheng/main/src/main/java/benjiecheng/App.java
@@ -0,0 +1,25 @@
+package benjiecheng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public  class App {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+	
+    public static void main(String[] args) {
+		// List<Integer> a= new ArrayList<Integer>();
+		// for (int i = 0; i < 10; i++) {
+		// 	a.add(i);
+		// } 
+		Integer a =10;
+		System.out.println(a.toString());
+    }
+  
+}
