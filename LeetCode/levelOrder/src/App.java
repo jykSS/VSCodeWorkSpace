a70616d983cbde7a0ce03a807efea19d0c5fe30b
jykSS <1010158370@qq.com>
2021-04-29 11:55:17 +0800

    最大公约数和204计数质数

--- LeetCode/levelOrder/src/App.java
+++ LeetCode/levelOrder/src/App.java
@@ -1,12 +1,24 @@
 import java.util.*;
 
-import javax.management.Query;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a =2;
+        int b = 3;
+       int  c = gcd(a,b);
+       System.out.println(c);
     }
-
+    /**
+     * 返回最大公约数
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
             if (root == null) {
