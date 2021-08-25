62aad5cc272c6cf37d2b35ac161beef88bb60330
jykSS <1010158370@qq.com>
2021-08-25 20:08:57 +0800

    一些leetcode

--- LeetCode/357.计算各个位数不同的数字个数.java
+++ LeetCode/357.计算各个位数不同的数字个数.java
@@ -22,6 +22,46 @@ class Solution {
             default : return 8877691;
         }
     }
+    
+}
+public class Solution {
+	public static int countNumbersWithUniqueDigits(int n) {
+		if (n > 10) {
+			return countNumbersWithUniqueDigits(10);
+		}
+		int count = 1; // x == 0
+		long max = (long) Math.pow(10, n);
+
+		boolean[] used = new boolean[10];
+
+		for (int i = 1; i < 10; i++) {
+			used[i] = true;
+			count += search(i, max, used);
+			used[i] = false;
+		}
+
+		return count;
+	}
+
+	private static int search(long prev, long max, boolean[] used) {
+		int count = 0;
+		if (prev < max) {
+			count += 1;
+		} else {
+			return count;
+		}
+
+		for (int i = 0; i < 10; i++) {
+			if (!used[i]) {
+				used[i] = true;
+				long cur = 10 * prev + i;
+				count += search(cur, max, used);
+				used[i] = false;
+			}
+		}
+
+		return count;
+	}
 }
 // @lc code=end
 
