50b88b2241f4fb7c519b15d920dc51eee22e24d9
jykSS <1010158370@qq.com>
2021-08-28 20:58:56 +0800

    leetcode

--- LeetCode/94.二叉树的中序遍历.java
+++ LeetCode/94.二叉树的中序遍历.java
@@ -26,13 +26,13 @@ class Solution {
 
         Stack<TreeNode> stack = new Stack<TreeNode>();
         while(root!=null || !stack.empty()){
-            while(root!=null){
-                stack.add(root);
-                root = root.left;
+            while (root != null) {
+                stack.push(root);
+                root= root.left;
             }
-            root = stack.pop();
+            root=stack.pop();
             list.add(root.val);
-            root = root.right;
+            root= root.right;
         }
         return list;
     }
