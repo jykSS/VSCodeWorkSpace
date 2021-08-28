f66508ad286828084de4c58b772a99d329b31c9d
jykSS <1010158370@qq.com>
2021-08-28 19:08:30 +0800

    一些leetcode更改

--- LeetCode/165.比较版本号.java
+++ LeetCode/165.比较版本号.java
@@ -9,35 +9,16 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-        int minLength = Math.min(v1.length, v2.length);
-        for (int i = 0; i < minLength; i++) {
-            if (Integer.parseInt(v1[i]) == Integer.parseInt(v2[i])) {
-                continue;
-            } else if (Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])) {
+        int maxLength = Math.max(v1.length, v2.length);
+        for (int i = 0; i < maxLength; i++) {
+            // v1
+            int j = (i < v1.length ? Integer.parseInt(v1[i]) : 0);
+            // v2
+            int k = (i < v2.length ? Integer.parseInt(v2[i]) : 0);
+            if (j < k) {
                 return -1;
-            } else {
+            } else if (j > k) {
                 return 1;
-            }
-        }
-        if (v1.length == v2.length) {
-            return 0;
-        }
-        StringBuilder temp = new StringBuilder("");
-        if (v1.length > minLength) {
-            for (int i = minLength; i < v1.length; i++) {
-                temp.append(v1[i]);
-            }
-            String temp1 = temp.toString().replaceAll("\\.", "");
-            if (Integer.parseInt(temp1) > 0) {
-                return 1;
-            }
-        } else {
-            for (int i = minLength; i < v2.length; i++) {
-                temp.append(v2[i]);
-            }
-            String temp1 = temp.toString().replaceAll("\\.", "");
-            if (Integer.parseInt(temp1) > 0) {
-                return -1;
             }
         }
         return 0;
