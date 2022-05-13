1e127e32837e3606df83a97650a16e78ea5c959b
ykjiang <1010158370@qq.com>
2022-05-13 22:36:09 +0800

    一道算法,岛屿数量

--- LeetCode/200.岛屿数量.java
+++ LeetCode/200.岛屿数量.java
@@ -15,7 +15,7 @@ class Solution {
         int y = grid[0].length;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == '1') {
                     res++;
                     infect(grid, i, j, x, y);
                 }
@@ -24,10 +24,10 @@ class Solution {
         return res;
     }
    private void infect(char[][] grid,int i , int j , int x , int y){
-       if (i<0||i>x||j<0||j>y||grid[i][j]!=1) {
+       if (i<0||i>=x||j<0||j>=y||grid[i][j]=='0') {
            return;
        }
-       grid[i][j]=2;
+       grid[i][j]='0';
        infect(grid, i+1, j, x, y);
        infect(grid, i-1, j, x, y);
        infect(grid, i, j+1, x, y);
